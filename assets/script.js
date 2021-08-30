f310ec18e8c3ce9ab862597ea3ea71266b623fae
Aaron Cox <aaroncox@Aarons-iMac.local>
2021-08-29 19:25:40 -0600

    final push

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
 
 // creating the current time 
 var rightNow = moment().format("MMMM Do, YYYY kk:mm");
-var storageTime = $(this).parent().siblings(".hour").children().text()
+//var storageTime = $(this).parent().siblings(".hour").children().text()
 var storageInput = $(".text").val();
-var storageInput = $(this).parent().siblings(".description").children().val();
+//var storageInput = $(this).parent().siblings(".description").children().val();
 
 
 
@@ -26,21 +26,27 @@ $(".time-block").each(function () {
 });
 
 
-var saveAppt = {
-    time: storageTime,
-    input: storageInput
-};
-var loadStorage = JSON.parse(localStorage.getItem("appointments"))
 
 
+// getting items from local storage!
+$("#9am").val(localStorage.getItem("09:00"));
+$("#10am").val(localStorage.getItem("10:00"));
+$("#11am").val(localStorage.getItem("11:00"));
+$("#12pm").val(localStorage.getItem("12:00"));
+$("#1pm").val(localStorage.getItem("13:00"));
+$("#2pm").val(localStorage.getItem("14:00"));
+$("#3pm").val(localStorage.getItem("15:00"));
+$("#4pm").val(localStorage.getItem("16:00"));
+$("#5pm").val(localStorage.getItem("17:00"));
 
-$('.saveBtn').click(function (saveAppt) {
+
+$('.saveBtn').click(function () {
     var storageInput = $(this).parent().siblings(".description").children().val();
     console.log(storageInput)
-    var storageTime = $(this).parent().siblings(".hour").children().text()
+    var storageTime = $(this).parent().siblings(".hour").children().text();
     console.log(storageTime)
-    localStorage.setItem("appointments", saveAppt)
-    //localStorage.setItem(storageTime, storageInput);
+
+    localStorage.setItem(storageTime, storageInput);
 });
 
 
